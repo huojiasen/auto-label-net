a7d58cabf42f74dcf96cfbecb26556cf98406df9
huojiasen <jiasen_huo@126.com>
2021-08-14 15:42:24 +0800

    Add files via upload

---
+++ infer.py
@@ -0,0 +1,140 @@
+import sys
+import shutil
+import click
+
+import mlflow
+import tensorflow as tf
+from tensorflow.saved_model import tag_constants
+from tensorflow.saved_model import signature_constants
+
+
+from model.nms_wrapper import nms
+from model.nms_wrapper import soft_nms
+
+import numpy as np
+import os, cv2
+from tqdm import tqdm
+
+from time import gmtime, strftime
+import json
+
+MULTIHEAD_NAME_LIST = ["shape_prob", "color_prob", "bbox_pred", "rois"]
+SINGLEHEAD_NAME_LIST = ["cls_prob", "bbox_pred", "rois"]
+
+def matching_three(a_entries, b_entries, c_entries):
+
+  res_entries = []
+  for a_ent in a_entries:
+    for b_ent in b_entries:
+      for c_ent in c_entries:
+        if a_ent[3:] == b_ent[3:] and a_ent[3:] == c_ent[3:]:
+          res_entries.append([a_ent[0], a_ent[1], b_ent[1], c_ent[1], a_ent[2],
+                              b_ent[2], c_ent[2], a_ent[3], a_ent[4], a_ent[5], a_ent[6]])
+  return res_entries
+
+def write_json(result_dir, entries):
+  objects = []
+  json_format = dict()
+  json_format['version'] = 0.0
+  json_format['tags'] = []
+  image_name = ""
+  for ent in entries:
+    for i in range(4, 11):
+      ent[i] = float(ent[i])
+    obj_struct = dict()
+    obj_struct['x'] = ent[7]
+    image_name = ent[0]
+    obj_struct['y'] = ent[8]
+    obj_struct['width'] = ent[9] - ent[7]
+    obj_struct['height'] = ent[10] - ent[8]
+    obj_struct['color_condfidence'] = ent[4]
+    obj_struct['shape_confidence'] = ent[5]
+    obj_struct['merge_confidence'] = ent[6]
+    obj_struct['annotation'] = {
+      "type": ent[2],
+      "color": ent[1],
+      "text": '-'
+    }
+    objects.append(obj_struct)
+  json_format['objects'] = objects
+
+  dst_path = os.path.join(result_dir, image_name[:-3] + "json")
+  with open(dst_path, 'w') as f:
+    json.dump(json_format, f)
+
+def split_detections_multihead(sess, tensor_dict, image_folder, image_name, result_dir, nms_method='hard'):
+    im_path = os.path.join(image_folder, image_name)
+    im = cv2.imread(im_path)
+    color_scores, shape_scores, boxes = im_detect_multihead_pb(sess, tensor_dict, im)
+    final_scores = merge_prob(color_scores, shape_scores)
+
+    color_entries = get_entries(image_name, boxes, color_scores, COLOR_CLASSES, 0.5)
+    shape_entries = get_entries(image_name, boxes, shape_scores, SHAPE_CLASSES, 0.5)
+    final_entries = get_entries(image_name, boxes, final_scores, CLASSES, 0.5)
+    filter_entries = matching_three(color_entries, shape_entries, final_entries)
+    write_json(result_dir, filter_entries)
+
+def get_input_output_tensor_dict(model):
+  tensor_dict = {}
+  inputs = dict(model.signature_def[signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY].inputs)
+  outputs = dict(model.signature_def[signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY].outputs)
+
+  merge_dict = inputs.copy()
+  merge_dict.update(outputs)
+  for key, val in merge_dict.items():
+    tensor = tf.get_default_graph().get_tensor_by_name(val.name)
+    if tensor is None:
+      raise TypeError("Expecting tensor " + val.name + " doesn't exist., The key is " + key)
+    tensor_dict[key] = tensor
+  return  tensor_dict
+
+def run_eval(demonet, tfmodel, im_folder, im_list, result_dir):
+  cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+  cfg.TEST.TESTING_SABE_PBTXT = True
+
+  # set config
+  tfconfig = tf.ConfigProto(allow_soft_placement=True)
+  tfconfig.gpu_options.allow_growth = True
+
+  # init session
+  sess = tf.Session(config=tfconfig)
+  model = tf.saved_model.loader.load(export_dir=tfmodel, sess=sess, tags=[tag_constants.SERVING])
+
+  tensor_dict = get_input_output_tensor_dict(model)
+
+  print('Loaded network {:s}'.format(tfmodel))
+  count = 0
+  with open(im_list, "r") as file:
+    content = file.readlines()
+    for i in tqdm(content):
+      im_name = i.strip() + ".jpg"
+      print(im_name)
+      print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+      if cfg.TASK.MODEL_TASK ==  "Multi":
+        split_detections_multihead(sess, tensor_dict, im_folder, im_name, result_dir)
+  sess.close()
+  mlflow.log_artifacts(result_dir, "infer_result")
+
+@click.command()
+def infer(data_folder, list_folder, infer_folder, model, config):
+  update_config(config, data_folder, list_folder)
+  image_folder = os.path.join(cfg.dataset.DATA_DIR, cfg.dataset.IMAGE_DIR)
+  eval_list = os.path.join(cfg.dataset.LIST_DIR, cfg.dataset.test_list)
+  demonet = 'res101'
+  if os.path.exists(infer_folder):
+    shutil.rmtree(infer_folder)
+  os.makedirs(infer_folder)
+
+  if os.path.exists(infer_folder + "/split"):
+      shutil.rmtree(infer_folder + "/split")
+  os.makedirs(infer_folder + "/split")
+
+  visual_dir = infer_folder + "/visual"
+  if os.path.exists(visual_dir):
+    shutil.rmtree(visual_dir)
+  os.mkdir(visual_dir)
+
+  run_eval(demonet, model, image_folder, eval_list, infer_folder)
+
+if __name__ == '__main__':
+  infer()
---
+++ train.py
@@ -0,0 +1,153 @@
+import os
+import sys
+import tensorflow as tf
+import click
+import pprint
+import mlflow
+
+
+from config import cfg, update_config
+from utils.timer import Timer
+from reader import get_blobs_func
+from tf_estimator.session_log_hook import SessionLogHook, SessionLogEndHook
+
+worker_index_id = 0
+num_gpus = 1
+
+
+def get_input_fn_truple(num_workers=1, worker_index=0):
+  image_list = os.path.join(cfg.dataset.LIST_DIR,
+                            cfg.dataset.train_list)
+  print('Read image list: ', image_list, num_workers, worker_index)
+  dataset = tf.data.TextLineDataset(image_list)
+  dataset = dataset.shard(num_workers, worker_index)
+
+  dataset = dataset.shuffle(10000)
+  dataset = dataset.repeat()
+
+  dataset = dataset.map(lambda image_id:
+                        tuple(tf.py_func(get_blobs_func, [image_id],
+                                         [tf.float32, tf.float32,
+                                          tf.float32, tf.string])),
+                        num_parallel_calls=2)
+  dataset = dataset.filter(lambda x, y, z, s:
+                           tf.not_equal(tf.shape(x)[0], 0))
+
+  dataset = dataset.prefetch(20)
+
+  iterator = dataset.make_one_shot_iterator()
+
+  image, im_info, label, image_id = iterator.get_next()
+  image.set_shape([1, None, None, 3])
+  im_info.set_shape([3])
+  label.set_shape([None, 5])
+  return image, im_info, label, image_id, worker_index
+
+
+def model_fn(features, labels, mode):
+  global worker_index_id
+  ''' Contruct graph '''
+  tf.set_random_seed(cfg.RNG_SEED)
+
+  image, im_info, label, image_id, worker_index = get_input_fn_truple(
+    num_workers=num_gpus, worker_index=worker_index_id)
+  worker_index_id += 1
+
+  from nets.resnet_v1 import resnetv1
+  net = resnetv1(num_layers=cfg.RESNET.num_layers)
+  layers = net.create_architecture('TRAIN', cfg.dataset.NUM_CLASSES,
+                                   tag='default',
+                                   anchor_scales=cfg.ANCHOR_SCALES,
+                                   anchor_ratios=cfg.ANCHOR_RATIOS,
+                                   image=image,
+                                   im_info=im_info,
+                                   gt_boxes=label)
+
+  loss = layers['total_loss']
+  global_step = tf.train.get_or_create_global_step()
+
+  base_lr = cfg.TRAIN.LEARNING_RATE * num_gpus
+  warmup_iters = 1000
+  lr = tf.train.polynomial_decay(
+    base_lr, global_step,
+    decay_steps=cfg.TRAIN.MAX_ITERS,
+    end_learning_rate=0, power=0.9,
+    name='learning_rate')
+
+  lr = tf.minimum(lr, base_lr / warmup_iters * tf.to_float(global_step))
+
+  optimizer = tf.train.MomentumOptimizer(lr, cfg.TRAIN.MOMENTUM)
+  optimizer = tf.contrib.estimator.TowerOptimizer(optimizer)
+  # Compute the gradients with regards to the loss
+
+  # train_op = optimizer.minimize(loss, tf.train.get_global_step())
+  gvs = optimizer.compute_gradients(loss)
+  train_op = optimizer.apply_gradients(gvs, global_step)
+
+  # restore from finetune model
+  exclude = ['global_step', 'learning_rate']
+  exclude += ['tower_{}/learning_rate'.format(i) for i in range(num_gpus)]
+  variables_to_restore = tf.contrib.slim.get_variables_to_restore(
+    exclude=exclude)
+  tf.train.init_from_checkpoint(cfg.TRAIN.finetune_model,
+                                {v.name.split(':')[0]: v for v in variables_to_restore}
+                                )
+
+  tf.summary.scalar('learning_rate', lr)
+
+  return tf.estimator.EstimatorSpec(
+    mode=tf.estimator.ModeKeys.TRAIN,
+    loss=loss,
+    train_op=train_op)
+
+
+@click.command()
+def train(data_folder, list_folder, enable_summary, config, gpus):
+  update_config(config, data_folder, list_folder)
+  os.environ['CUDA_VISIBLE_DEVICES'] = gpus
+  global num_gpus
+  num_gpus = len(gpus.split(','))
+
+  image_list = os.path.join(cfg.dataset.LIST_DIR,
+                            cfg.dataset.train_list)
+  with open(image_list) as f:
+    image_num = len(f.readlines())
+  num_per_epoch = image_num / num_gpus
+
+  cfg.TRAIN.MAX_ITERS = num_per_epoch * 15
+
+  mlflow.log_param('train', cfg.TRAIN)
+  pprint.pprint(cfg)
+
+  tf.logging.set_verbosity(tf.logging.INFO)
+  run_config = tf.estimator.RunConfig(
+    save_checkpoints_steps=cfg.TRAIN.snapshot_interval,
+    log_step_count_steps=(cfg.TRAIN.log_interval if enable_summary else cfg.TRAIN.MAX_ITERS),
+    keep_checkpoint_max=cfg.TRAIN.snapshot_max_to_keep,
+    session_config=tf.ConfigProto(
+      allow_soft_placement=True,
+      gpu_options=tf.GPUOptions(force_gpu_compatible=True, allow_growth=True))
+  )
+
+  mod_fn = model_fn
+  if num_gpus > 1:
+    mod_fn = tf.contrib.estimator.replicate_model_fn(
+      mod_fn,
+      loss_reduction=tf.losses.Reduction.MEAN)
+
+  estimator = tf.estimator.Estimator(
+    model_fn=mod_fn,
+    model_dir=cfg.TRAIN.snapshots_dir,
+    config=run_config,
+  )
+
+  session_log_hook = SessionLogHook(cfg.TRAIN.log_interval)
+  estimator.train(input_fn=lambda: ([0] * num_gpus, [0] * num_gpus),
+                  hooks=[session_log_hook],
+                  max_steps=cfg.TRAIN.MAX_ITERS)
+
+  mlflow.log_artifact(cfg.TRAIN.snapshots_dir + '/checkpoint', "train_model")
+
+
+if __name__ == '__main__':
+  train()
